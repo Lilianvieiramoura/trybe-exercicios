e22293dee785a93e055cce723f3d09554d9254ce
Lílian Vieira <lilianv528@gmail.com>
2022-10-20 15:10:21 -0300

    exercicio 1 finalizado

---
+++ fundamentos/secao-04-introducao-a-java-script/ex-variaveis/variaveis.js
@@ -0,0 +1,12 @@
+const myName = "Lilian";
+const birthCity = "Barreiros/PE";
+
+let birthYear = 2001;
+birthYear = 2030;
+
+console.log(myName);
+
+console.log(birthCity);
+
+console.log(birthYear);
+
