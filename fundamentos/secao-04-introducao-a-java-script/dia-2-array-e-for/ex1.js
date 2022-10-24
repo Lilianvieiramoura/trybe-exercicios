289b24aea7d4e90208fd6f9c52ae413d774c84c7
Lílian Vieira <lilianv528@gmail.com>
2022-10-24 19:11:24 -0300

    operação aritmetica

---
+++ fundamentos/secao-04-introducao-a-java-script/dia-2-array-e-for/ex1.js
@@ -0,0 +1,9 @@
+const a = 20;
+const b = 5;
+
+console.log('Adição: ' + (a+b));
+console.log('Subtração: ' + (a-b));
+console.log('Multiplicação: ' + (a*b));
+console.log('Divisão: ' + (a/b));
+console.log('Módulo: ' + (a%b));
+
