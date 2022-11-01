e3a5861ca81073f5d16badfa4be99ce6fe478bc5
Lílian Vieira <lilianv528@gmail.com>
2022-11-01 15:57:44 -0300

    ex 1

---
+++ fundamentos/secao-04-introducao-a-java-script/dia1-primeiros-passos/exercicios.js
@@ -0,0 +1,11 @@
+const adição = 50 + 5;
+console.log(adição);
+
+const subtracao = 50 - 5;
+console.log(subtracao);
+
+const multiplicacao = 50 * 5;
+console.log(multiplicacao);
+
+const divisao = 50 / 5;
+console.log(divisao);
